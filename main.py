81eeaa84bba89f7e92c5a21ab32092b026ccbeb7
Parham Alvani <parham.alvani@gmail.com>
2025-05-10 03:41:28 +0000

    feat: print public key

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 
 def main():
     private_key = Ed25519PrivateKey.generate()
+    public_key = private_key.public_key()
 
     b = private_key.private_bytes_raw()
-    print(base64.b64encode(b))
+    print(f'private key: {base64.b64encode(b)}')
+    b = public_key.public_bytes_raw()
+    print(f'public key: {base64.b64encode(b)}')
 
     now = time.time_ns()
     print(f"now: {now}")
 
     signature = private_key.sign(f"GET /api/v4/prices {now}".encode())
-    public_key = private_key.public_key()
     public_key.verify(signature, f"GET /api/v4/prices {now}".encode())
 
 
